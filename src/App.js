afd9c3bfd1877f0a609b5f59f8bac85bc66db904
Kate Santo <katerine.santo@gmail.com>
2019-10-24 15:50:04 +0100

    App now works again(didn't work in previous commit

--- src/App.js
+++ src/App.js
@@ -23,34 +23,35 @@ class App extends React.Component {
       description: "Buy wood shavings",
       completed: false
     }
-  
+
     ];
     return (
       <div>
         {
-          tasks.map(function(task, index) {
+          tasks.map(function (task, index) {
             return <activeTask task={task} key={index} />;
           })
         }
       </div>
     );
-}
-}
-function App() {
-  return (
-    <div className="container">
-      <h1><Header headerDescription="Get stuff done" /></h1>
-      <Newtask />
-      <h2><Header headerDescription="Active tasks" /></h2>
-      <ActiveTask taskDescription="Buy some milk" />
-      <ActiveTask taskDescription="Buy dog food" />
-      <ActiveTask taskDescription="Buy chocolate" />
-      <ActiveTask taskDescription="Do homework" />
-      <h2><Header headerDescription="Completed tasks" /></h2>
-      <CompletedTask completedTaskDescription="Buy guinea pig food" />
-      <CompletedTask completedTaskDescription="Refill hand soap" />
-    </div>
-  );
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1><Header headerDescription="Get stuff done" /></h1>
+        <Newtask />
+        <h2><Header headerDescription="Active tasks" /></h2>
+        <ActiveTask taskDescription="Buy some milk" />
+        <ActiveTask taskDescription="Buy dog food" />
+        <ActiveTask taskDescription="Buy chocolate" />
+        <ActiveTask taskDescription="Do homework" />
+        <h2><Header headerDescription="Completed tasks" /></h2>
+        <CompletedTask completedTaskDescription="Buy guinea pig food" />
+        <CompletedTask completedTaskDescription="Refill hand soap" />
+      </div>
+    );
+  }
 }
 
 export default App;
