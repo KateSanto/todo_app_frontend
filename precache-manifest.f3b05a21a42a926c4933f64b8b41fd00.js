cb33eb0f41c6d87ae346f4cbc1617f1b03ff47ea
Kate Santo <katerine.santo@gmail.com>
2019-10-31 10:16:19 +0000

    Updates

---
+++ precache-manifest.f3b05a21a42a926c4933f64b8b41fd00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17afb39451a874cf25a5da08770dfce",
+    "url": "/todo_app_frontend/index.html"
+  },
+  {
+    "revision": "354d4a1d69fe76327a0c",
+    "url": "/todo_app_frontend/static/css/main.4d5eb024.chunk.css"
+  },
+  {
+    "revision": "1f1b6de77c0e72fd2964",
+    "url": "/todo_app_frontend/static/js/2.113faf43.chunk.js"
+  },
+  {
+    "revision": "354d4a1d69fe76327a0c",
+    "url": "/todo_app_frontend/static/js/main.f97ebcc5.chunk.js"
+  },
+  {
+    "revision": "3fe3d5b967a993183ded",
+    "url": "/todo_app_frontend/static/js/runtime-main.055414d6.js"
+  }
+]);
