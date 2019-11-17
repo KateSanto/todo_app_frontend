c2d664f9bba0f9f6b705b612dabc5a89aa7c1635
Kate Santo <katerine.santo@gmail.com>
2019-11-17 12:30:02 +0000

    Updated database.sql file. Completed

--- Documents/database.sql
+++ Documents/database.sql
@@ -1,3 +1,13 @@
+CREATE DATABASE get_stuff_done;
+
+USE get_stuff_done;
+
+
+CREATE TABLE user (userId BIGINT(20) NOT NULL AUTO_INCREMENT, name VARCHAR(50), surname VARCHAR(50), PRIMARY KEY(userId));
+
+INSERT INTO user (userId, name, surname) VALUES ("1", "Kate", "Santo");
+
+INSERT INTO user (userId, name, surname) VALUES ("2", "Sofia", "Herranz");
 
 SELECT * FROM user;
 +--------+-------+---------+
@@ -7,6 +17,19 @@ SELECT * FROM user;
 |      2 | Sofia | Herranz |
 +--------+-------+---------+
 
+
+
+CREATE TABLE task (id VARCHAR(50), taskDescription VARCHAR(100), completed VARCHAR(20), creationDate DATE, userId BIGINT(20), PRIMARY KEY (id));
+
+ALTER TABLE task ADD FOREIGN KEY (userId) REFERENCES user(userId);
+
+INSERT INTO task (id, taskDescription, completed, creationDate, userId) VALUES ("001", "Buy milk ", "false", "2019-11-16", "1");
+
+INSERT INTO task (id, taskDescription, completed, creationDate, userId) VALUES ("002", "Buy bread", "false", "2019-11-16", "1");
+
+INSERT INTO task (id, taskDescription, completed, creationDate, userId) VALUES ("003", "Wash car", "false", "2019-11-15", "1");
+
+INSERT INTO task (id, taskDescription, completed, creationDate, userId) VALUES ("004", "Feed piggies", "false", "2019-11-16", "2");
 
 SELECT * FROM task;
 +------+-----------------+-----------+--------------+--------+
